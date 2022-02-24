f88e83ea34cba9995679596cb0613c917e8643f4
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2022-02-24 08:25:57 +0400

    running dev command concurrently

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   purge: { content: ["./dist/**/*.html", "./dist/*.html"], enabled: true },
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
   theme: {
     extend: {},
   },
